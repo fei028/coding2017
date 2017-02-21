ad5e745ac4038afa0590ae384f8797135883384f
xfei <xfei028@gmail.com>
2017-02-21 09:21:28 +0800

    Signed-off-by: xfei <xfei028@gmail.com>

--- group01/2137642225/src/com/coding/mybasic/List.java
+++
@@ -1,9 +0,0 @@
-package com.coding.mybasic;
-
-public interface List {
-	public void add(Object element);
-	public void add(int index, Object element) throws Exception;
-	public Object get(int index);
-	public Object remove(int index);
-	public int size();
-}
--- group01/2137642225/src/com/coding/mybasic/Test.java
+++
@@ -1,8 +0,0 @@
-package com.coding.mybasic;
-
-public class Test {
-
-	public static void main(String[] args) {
-		new ArrayList().add(1, "");
-	}
-}

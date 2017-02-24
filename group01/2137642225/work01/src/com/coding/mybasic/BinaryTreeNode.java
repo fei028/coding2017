74f20572bc5503168696e81c92aa5e2120f5f1ce
xfei <xfei028@gmail.com>
2017-02-24 12:02:18 +0800

    tree

--- group01/2137642225/work01/src/com/coding/mybasic/BinaryTreeNode.java
+++ group01/2137642225/work01/src/com/coding/mybasic/BinaryTreeNode.java
@@ -2,14 +2,14 @@ package com.coding.mybasic;
 
 public class BinaryTreeNode {
 	
-	private Object data;
+	private Integer data;
 	private BinaryTreeNode left;
 	private BinaryTreeNode right;
 	
 	public Object getData() {
 		return data;
 	}
-	public void setData(Object data) {
+	public void setData(Integer data) {
 		this.data = data;
 	}
 	public BinaryTreeNode getLeft() {
@@ -25,26 +25,47 @@ public class BinaryTreeNode {
 		this.right = right;
 	}
 	
-	public BinaryTreeNode insert(Object o){
-		search(this,o);
-		BinaryTreeNode binaryTreeNode = new BinaryTreeNode();
-		binaryTreeNode.setData(o);
-		return null ;
+	public BinaryTreeNode insert(Integer o){
+		if(o == null){
+			throw new RuntimeException("不能插入空值");
+		}
+		BinaryTreeNode searchNode = search(this,o);
+		if(isExistData(searchNode,o)){
+			throw new RuntimeException("该值已存在 无法插入");
+		}
+		if(searchNode != null){
+			BinaryTreeNode binaryTreeNode = new BinaryTreeNode();
+			binaryTreeNode.setData(o);
+			if(searchNode.data.intValue() > o.intValue()){
+				searchNode.setLeft(binaryTreeNode);
+			}else{
+				searchNode.setRight(binaryTreeNode);
+			}
+		} else {
+			this.setData(o);
+		}
+		return this;
 	}
-	private boolean isExistData(Object data) {
-		return search(this,data) != null;
+	
+	private boolean isExistData(BinaryTreeNode searchNode,Integer data) {
+		return  searchNode != null && searchNode.data.intValue() == data.intValue();
 		
 	}
 	
-	private BinaryTreeNode search(BinaryTreeNode binaryTreeNode, Object data) {
-		if(binaryTreeNode == null){
+	private BinaryTreeNode search(BinaryTreeNode binaryTreeNode, Integer data) {
+		if(binaryTreeNode == null || binaryTreeNode.data == null){
 			return null;
 		}
-		Object curNodeData = binaryTreeNode.data;
-		if(curNodeData != null){// 左 curNodeData < data
-			search(binaryTreeNode.left,data);
-		}else if(curNodeData == null){
-			search(binaryTreeNode.right,data);
+		Integer curNodeData = binaryTreeNode.data;
+		if(curNodeData.intValue() > data.intValue()){// 左 curNodeData > data
+			if(binaryTreeNode.left != null){
+				return search(binaryTreeNode.left,data);
+			} 
+		}else if(curNodeData.intValue() < data.intValue()){
+			if(binaryTreeNode.right != null){
+				return search(binaryTreeNode.right,data);
+			} 
+			
 		}
 		return binaryTreeNode;
 	}
